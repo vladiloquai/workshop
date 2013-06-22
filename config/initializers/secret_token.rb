f9cf3ed3981319aec8f41c6506a929f2543bc3d8
vla <vladimir@acid.cl>
2013-06-22 09:08:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = '9e782b12d998e8d85069ebdb277db46b1795a7f2bd255a3aa760f12cd4ad5bc6b9a25b7b259d0dda0c497333fce32664dc61b065fd7c645b1ad6f829ffb590bf'
