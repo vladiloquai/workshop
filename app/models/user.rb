c390eda60a0f271d48da1ded2486d83fe23de531
vla <vladimir@acid.cl>
2013-06-22 12:13:38 -0400

    setup for ability and user classes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,28 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  attr_accessible :analyst_id, :client_id, :email, :email, :lastname, :name, :role
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+                  :analyst_id, :client_id, :email, :email, :lastname, :name, :role
+
+  validates :email, 
+            format: { with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, 
+                      message: I18n.t('messages.invalid')}
+
+  # Roles Configuration
+  ROLES = %w[admin analyst client]
+
+  # The URL of the role
+  ROLES_URL = %w[
+      /incidents
+      /incidents
+      /incidents
+  ]
+
+  def role? _role 
+    if self.role
+      self.role.to_sym == _role
+    else
+      false
+    end
+  end
 end
