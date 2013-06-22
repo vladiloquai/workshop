2e8d55fc4c2fa5fc863173b8f5f1625f36aa64d0
vla <vladimir@acid.cl>
2013-06-22 09:46:52 -0400

    add schema.rb and fix create_users migration

--- db/migrate/20130622132502_create_users.rb
+++ db/migrate/20130622132502_create_users.rb
@@ -1,12 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :name
-      t.string, :lastname
-      t.string :email
-      t.string, :email
-      t.string, :role
-      t.integer, :client_id
+      t.string :name
+      t.string :lastname
+      t.string :role
+      t.integer :client_id
       t.integer :analyst_id
 
       t.timestamps
