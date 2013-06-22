7e3c9adca5f105fa9f741580c9a522913fb1e098
vla <vladimir@acid.cl>
2013-06-22 13:58:33 -0400

    add more users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
+    raise ""
+
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
