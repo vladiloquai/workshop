8a7201b1a9c83bdf3189a6e32a7d0b1b84c89171
vla <vladimir@acid.cl>
2013-06-26 11:55:15 -0400

    Run migration to caterogies table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130626124323) do
+ActiveRecord::Schema.define(:version => 20130626154730) do
 
   create_table "analysts", :force => true do |t|
     t.integer  "status"
@@ -19,9 +19,24 @@ ActiveRecord::Schema.define(:version => 20130626124323) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
   create_table "clients", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "incident_categories", :force => true do |t|
+    t.integer  "incident_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "incident_users", :force => true do |t|
